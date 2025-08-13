ee9fcb415c321cde271f25ca38803cd152c5a3ad
Fabi <fabiane1974@hotmail.com>
2025-08-13 14:35:37 -0300

    commit inicial

---
+++ src/test/java/com/fabianesilva/angendamento_notificacao_api/AngendamentoNotificacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabianesilva.angendamento_notificacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngendamentoNotificacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
